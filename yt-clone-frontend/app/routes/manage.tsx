1684e481c6f6aa5ddcec2f369464625017708e4f
jaca1119 <laki1663@gmail.com>
2026-03-28 12:01:04 +0100

    Update manage view

--- yt-clone-frontend/app/routes/manage.tsx
+++ yt-clone-frontend/app/routes/manage.tsx
@@ -3,6 +3,8 @@ import type { Route } from "./+types/manage";
 import Thumbnail from "~/videos-list/thumbnail";
 import { useFetcher } from "react-router";
 import { Link } from "react-router";
+import { Button, Table } from "@heroui/react";
+import dayjs from "dayjs";
 
 export async function clientLoader() {
   return getAllVideosForUser();
@@ -22,26 +24,57 @@ export default function Manage({ loaderData }: Route.ComponentProps) {
 
   return (
     <div>
-      <p>Manage</p>
-      <div className="flex flex-col gap-3">
-        {loaderData.map((v) => (
-          <div key={v.id} className="flex gap-2">
-            <Thumbnail
-              className="w-28"
-              videoId={v.id}
-              length={v.length}
-            ></Thumbnail>
-            <p>{v.title}</p>
+      <p className="font-bold text-xl">Manage</p>
+      <Table>
+        <Table.ScrollContainer>
+          <Table.Content className="min-w-150">
+            <Table.Header>
+              <Table.Column>Video</Table.Column>
+              <Table.Column>Title</Table.Column>
+              <Table.Column>Upload date</Table.Column>
+              <Table.Column>Actions</Table.Column>
+            </Table.Header>
+            <Table.Body>
+              {loaderData.map((v) => (
+                <Table.Row key={v.id}>
+                  <Table.Cell>
+                    <Thumbnail
+                      className="w-28"
+                      videoId={v.id}
+                      length={v.length}
+                    ></Thumbnail>
+                  </Table.Cell>
+                  <Table.Cell>{v.title}</Table.Cell>
+                  <Table.Cell>
+                    {dayjs(v.uploadDate).format("DD MMM YYYY")}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <div className="flex gap-1">
+                      <fetcher.Form method="DELETE">
+                        <input
+                          type="hidden"
+                          name="videoId"
+                          value={v.id}
+                        ></input>
+                        <Button variant="secondary" type="submit">
+                          Delete
+                        </Button>
+                      </fetcher.Form>
 
-            <fetcher.Form method="DELETE">
-              <input type="hidden" name="videoId" value={v.id}></input>
-              <button type="submit">Delete</button>
-            </fetcher.Form>
-
-            <Link to={`videos/${v.id}/edit`}>Edit</Link>
-          </div>
-        ))}
-      </div>
+                      <Link
+                        className="button button--secondary"
+                        to={`videos/${v.id}/edit`}
+                      >
+                        Edit
+                      </Link>
+                    </div>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Content>
+        </Table.ScrollContainer>
+      </Table>
     </div>
   );
 }
