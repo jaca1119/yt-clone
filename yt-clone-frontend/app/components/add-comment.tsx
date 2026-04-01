effa7b694505ba0af85389f89dfac47197f33a12
jaca1119 <laki1663@gmail.com>
2026-04-01 16:11:17 +0200

    Reset form after comment add

--- yt-clone-frontend/app/components/add-comment.tsx
+++ yt-clone-frontend/app/components/add-comment.tsx
@@ -1,5 +1,5 @@
 import { Button, Label, TextArea } from "@heroui/react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useFetcher } from "react-router";
 
 export default function AddComment({
@@ -13,8 +13,21 @@ export default function AddComment({
 }) {
   const fetcher = useFetcher();
   const [showControls, setShowControls] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (fetcher.state === "idle" && fetcher.data?.ok) {
+      if (formRef.current) {
+        formRef.current.reset();
+      }
+
+      setShowControls(false);
+      fetcher.reset();
+    }
+  }, [fetcher.state, fetcher.data, fetcher]);
+
   return (
-    <fetcher.Form method="POST" className="flex flex-col gap-2">
+    <fetcher.Form ref={formRef} method="POST" className="flex flex-col gap-2">
       <input type="hidden" name="videoId" value={videoId}></input>
       {replyId && <input type="hidden" name="replyId" value={replyId}></input>}
       <Label htmlFor="comment">{children || "Add comment:"}</Label>
