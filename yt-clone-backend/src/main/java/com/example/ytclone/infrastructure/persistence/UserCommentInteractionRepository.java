6201ab8e490c03b3a81e9e65044068ef5cc79d07
jaca1119 <laki1663@gmail.com>
2026-04-11 11:13:43 +0200

    Fix missing method

--- yt-clone-backend/src/main/java/com/example/ytclone/infrastructure/persistence/UserCommentInteractionRepository.java
+++ yt-clone-backend/src/main/java/com/example/ytclone/infrastructure/persistence/UserCommentInteractionRepository.java
@@ -3,8 +3,10 @@ package com.example.ytclone.infrastructure.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserCommentInteractionRepository extends JpaRepository<UserCommentInteractionEntity, UUID> {
     List<UserCommentInteractionEntity> findAllByUsernameAndCommentIdIn(String username, List<UUID> commentId);
+    Optional<UserCommentInteractionEntity> findByUsernameAndCommentId(String username, UUID commentId);
 }
